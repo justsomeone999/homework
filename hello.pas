60d8fe7ccc610451ee434ad5a6d775c9c2e9fa16
Maria Ligorova <masaligorova@gmail.com>
2024-10-09 19:41:31 +0300

    Languages

--- hello.pas
+++ hello.pas
@@ -1,3 +1,15 @@
 ﻿begin
-  
+  var A := ReadString('На каком языке вы говорите?');
+  if a = 'На русском' then
+    Print('Привет')
+  else if A = 'На английском' then
+    Print('Hello')
+  else if A = 'На итальянском' then
+    Print('Ciao')
+  else if A = 'На французском' then
+    Print('Salut')
+  else if A = 'На татарском' then
+    Print('Сәлам')
+  else 
+    Print('Программа не знает этого языка')
 end.
